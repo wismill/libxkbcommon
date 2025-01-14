3fc4c70f6dd971dce0d52f9771307dbfac45a637
Pierre Le Marre <dev@wismill.eu>
2025-01-14 17:14:26 +0100

    compat: Delete erroneous merge mode handling

--- src/xkbcomp/compat.c
+++ src/xkbcomp/compat.c
@@ -764,8 +764,6 @@ HandleCompatMapFile(CompatInfo *info, XkbFile *file, enum merge_mode merge)
 {
     bool ok;
 
-    merge = (merge == MERGE_DEFAULT ? MERGE_AUGMENT : merge);
-
     free(info->name);
     info->name = strdup_safe(file->name);
 
